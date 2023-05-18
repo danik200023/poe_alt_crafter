76eb43f978c4db3bd191bf32a737c2b05a5be8b1
danila <danik200023@proton.me>
2023-05-18 09:28:33 +0400

    +1 to all Skill Gems amulet check

---
+++ main.py
@@ -0,0 +1,38 @@
+import pyclip
+from pynput.mouse import Listener, Button
+import pyautogui
+
+
+block_left_click = False
+
+
+def on_click(x, y, button, pressed):
+    global block_left_click
+
+    if button == Button.left and block_left_click:
+        return False
+
+
+def analyze_item_description():
+    item_description = pyclip.paste().decode("UTF-8")
+    if "+1 to Level of all Skill Gems" in item_description:
+        print("Mouse blocked")
+        block_left_click = True
+
+
+clipboard_content = pyclip.paste()
+
+listener = Listener(on_click=on_click)
+listener.start()
+
+pyautogui.keyDown('shift')
+
+# Останавливаем слушателя (например, после выполнения определенных действий)
+
+
+# listener.stop()
+while True:
+    if pyclip.paste() != clipboard_content:
+        clipboard_content = pyclip.paste()
+        analyze_item_description()
+    pyautogui.click()
